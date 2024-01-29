4d4f47ffcaa52b84b2e990e6ff00008d2f6fa534
Pepega42 <144796765+Pepega42@users.noreply.github.com>
2024-01-29 08:28:36 +0300

    Add files via upload

---
+++ Bananchiki-tortiki/ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(" Заказ тортов");
+                Console.WriteLine("Выберите параметр");
+                Console.WriteLine("-----------------");
+                Console.WriteLine("  Выбор формы торта");
+                Console.WriteLine("  Выбор размера торта");
+                Console.WriteLine("  Выбор вкуса коржей");
+                Console.WriteLine("  Выбор количества коржей");
+                Console.WriteLine("  Выбор глазури");
+                Console.WriteLine("  Выбор декора");
+                Console.WriteLine("  Завершить заказ");
+                Console.WriteLine("Итоговая цена: " + Order.itogCost);
+                Console.WriteLine("Ваш торт: " + Order.itogName);
+                int pos = Menu.Show(3, 9);
+                Order order = new Order();
+                order.Or(pos);
+                if (pos == 9)
+                {
+                    ConsoleKeyInfo key;
+                    Console.WriteLine("Благодарим за заказ! Если хотите сделать ещё один, нажмите Escape");
+                    do
+                    {
+                        key = Console.ReadKey();
+                    } while (key.Key != ConsoleKey.Escape);
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
